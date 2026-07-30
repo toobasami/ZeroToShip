a086ef9858eb8aeb3b7f4be8ae7c9264ec1aafc7
Tooba Sami <toobasami1280@gmail.com>
2026-07-30 11:23:05 +0500

    Add Phase 3 Pitch model

---
+++ Phase-3/models/Pitch.h
@@ -0,0 +1,49 @@
+#ifndef PITCH_H
+#define PITCH_H
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Pitch
+{
+private:
+
+    int id;
+    int target_order_num;
+    string text;
+    string author;
+    string status;
+
+public:
+
+ Pitch(int id = 0, int target_order_num = 0, string text = "", string author = "", string status = "Pending")
+        : id(id),
+          target_order_num(target_order_num),
+          text(text),
+          author(author),
+          status(status)
+    {}
+
+    // Serialization
+    map<string, string> to_dict();
+
+    // Deserialization
+    static Pitch from_dict(const map<string, string>& data);
+
+    void setStatus(string newStatus){
+        status = newStatus;
+    }
+    
+    void display();
+
+    // Getters
+    int getId();
+    int getTargetOrderNum();
+    string getText();
+    string getAuthor();
+    string getStatus();
+};
+
+#endif
